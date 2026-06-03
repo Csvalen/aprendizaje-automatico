0a0d5ef91de0ab8a95048e7568e8ee7047cb3ba6
Csvalen <saraa.vv96@gmail.com>
2026-06-03 20:37:36 -0300

    Agrego proyecto_aprendizaje_automatico con Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicción del consumo de gas envasado en Tierra del Fuego utilizando variables climaticas y aprendizaje automatico.',
+    author='Sara Valenzuela',
+    license='',
+)
